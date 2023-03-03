2f563fd566c1ec1ea0de9f992b7b2eac4fc97596
Rosario Garcia <rosariogf041@gmail.com>
2023-03-03 17:06:28 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 100)
+let num3 = 4
+let num4 = 8
+const remainder = num3 % num4
+const mod = ((num3%num4)+num4)%num4
+console.log(mod)
+const max = Math.max(10, 20)
